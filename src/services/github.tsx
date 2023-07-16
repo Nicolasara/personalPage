4d093f282b639adb318b98949e43dcc92a56a1e2
Nicolas Araujo <araujo.n@northeastern.edu>
2023-07-15 17:32:42 -0700

    change github api to lambda backend

--- src/services/github.tsx
+++ src/services/github.tsx
@@ -1,23 +1,19 @@
 import Repo from '../types/Repo';
+import axios from 'axios';
 
 const url = require('url')
 
-export async function getAllRepos(pp: number, page: number, type="owner"): Promise<Repo[]> {
-    let urlObject = {
-        protocol: "https",
-        hostname: "api.github.com/user/repos",
-        query: {
-            per_page: pp,
-            page: page,
-            type: type,
-        },
-    };
-    console.log(url.format(urlObject));
-    const headers = {
-        "accept": "application/vnd.github.v3+json",
-        "Authorization": `token ${process.env.REACT_APP_TOKEN}`
+export async function getRepos(perPage: number, page: number, type = "owner"): Promise<Repo[]> {
+    const url = `https://ykdg2r22r1.execute-api.us-east-1.amazonaws.com/prod/repos/${perPage}/${page}`
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+        throw new Error("Error fetching repos");
+    } else {
+        const repos: Repo[] = response.data.map((repo: any) => new Repo(repo));
+        console.log(response.data);
+        console.log(repos);
+        return repos;
     }
-    return getPromise(url.format(urlObject), headers);
 }
 
 export async function getUser() {
@@ -32,15 +28,15 @@ export async function getUser() {
     return getPromise(url.format(urlObject), headers);
 }
 
-const getPromise = (url: RequestInfo, headers={}): Promise<any> => {
+const getPromise = (url: RequestInfo, headers = {}): Promise<any> => {
     return new Promise((resolve, reject) => {
         fetch(url, {
             method: "GET",
             headers: headers
         })
-        .then(res => res.json())
-        .then(data => {
-            resolve(data);
-        });
+            .then(res => res.json())
+            .then(data => {
+                resolve(data);
+            });
     });
 }
