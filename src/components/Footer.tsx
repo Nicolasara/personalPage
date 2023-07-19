8896a461fc95433aa9645cdcdafc1bd353717636
Nicolas Araujo <araujo.n@northeastern.edu>
2023-07-18 22:47:48 -0700

    Fix Footer Naming and Padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-function Header() {
+function Footer() {
     return (
-        <div className='flex w-full bg-slate-100 mt-20'>
+        <div className='flex w-full px-10 bg-slate-100 mt-20'>
             <div className='w-full my-10'>
-                <p className='inline-block my-3 ml-10'>Made in Boston</p>
+                <p className='inline-block my-3'>Made in Boston</p>
                 <div className="inline-block float-right my-3">
                     <a href='https://github.com/Nicolasara' target="_blank" rel="noreferrer">
-                        <img src='./github.png' alt='github' className="inline-block w-8 mr-3"/>
+                        <img src='./github.png' alt='github' className="inline-block w-8 mr-3" />
                     </a>
                     <a href='https://linkedin.com/in/nico-araujo' target="_blank" rel="noreferrer">
-                        <img src='./linkedin.png' alt='linkedin' className="inline-block w-8 mr-3"/>
+                        <img src='./linkedin.png' alt='linkedin' className="inline-block w-8 mr-3" />
                     </a>
                 </div>
             </div>
@@ -16,4 +16,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Footer
