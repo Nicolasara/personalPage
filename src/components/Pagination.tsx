ff66ed8119dccd8bf3b1abb3ea76412f496e7bd9
Nicolas Araujo <araujo.n@northeastern.edu>
2023-07-18 22:48:09 -0700

    Fix Pagination Padding

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function Pagination({ back, next, page }: Props) {
     return (
-        < div className='w-full text-center' >
+        <div className='w-full text-center my-10' >
             < div className="inline-block" >
                 <button onClick={back} className="inline-block py-3 px-5 bg-slate-200 rounded-md hover:bg-slate-100 transition-colors duration-300">back</button>
                 <h2 className="inline-block mx-8">{page}</h2>
