0e824ad771ddcf4eede838b6310d4d7f528fff3d
Nicolas Araujo <araujo.n@northeastern.edu>
2023-08-02 23:59:52 -0700

    add third test case

--- src/services/github.test.ts
+++ src/services/github.test.ts
@@ -1,25 +1,34 @@
-import { getRepos } from "./github"
+import { getRepos } from "./github";
 
 const TOTAL_REPOS = 10;
 
 describe("github", () => {
-    it("returns 200 status code", async () => {
-        expect(async () => await getRepos(1, 1)).not.toThrow("Error fetching repos");
-    })
+  it("returns 200 status code", async () => {
+    expect(async () => await getRepos(1, 1)).not.toThrow(
+      "Error fetching repos"
+    );
+  });
 
-    it("returns correct amount of repos per page", async () => {
-        const repoResponses = [];
-        for (let i = 1; i <= TOTAL_REPOS; i++) {
-            const repoResponse = getRepos(i, 1);
-            repoResponses.push(repoResponse);
-        }
+  it("returns correct amount of repos per page", async () => {
+    const repoResponses = [];
+    for (let i = 1; i <= TOTAL_REPOS; i++) {
+      const repoResponse = getRepos(i, 1);
+      repoResponses.push(repoResponse);
+    }
 
-        const repos = await Promise.all(repoResponses);
-        repos.forEach((repo, index) => {
-            expect(repo).toHaveLength(index + 1);
-        });
-    })
+    const repos = await Promise.all(repoResponses);
+    repos.forEach((repo, index) => {
+      expect(repo).toHaveLength(index + 1);
+    });
+  });
 
-    it("return different repos per page", async () => {
-    })
-})
+  it("return different repos per page", async () => {
+    const repoPageOneResponse = getRepos(1, 1);
+    const repoPageTwoResponse = getRepos(1, 2);
+    const [repoPageOne, repoPageTwo] = await Promise.all([
+      repoPageOneResponse,
+      repoPageTwoResponse,
+    ]);
+    expect(repoPageOne[0].name).not.toEqual(repoPageTwo[0].name);
+  });
+});
